5dc906ed87130ee5621a30cdd0980aabc3ea3e0f
Zaynab-T <ztabass2@students.towson.edu>
2025-11-07 15:04:55 -0500

    Added AppCompat and Material dependencies for new SettingsActivity class

---
+++ app/src/main/java/com/example/zentask/SettingsActivity.java
@@ -0,0 +1,11 @@
+package com.example.zentask;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity {
+}
