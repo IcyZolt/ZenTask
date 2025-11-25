84628fa62e8d438acfed3579808e440fdaa27e4e
Zaynab-T <ztabass2@students.towson.edu>
2025-11-24 19:50:41 -0500

    made page for archived tasks

---
+++ app/src/main/java/com/example/zentask/TaskLogic/ArchivedTasks.java
@@ -0,0 +1,44 @@
+package com.example.zentask.TaskLogic;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.zentask.R;
+
+import java.util.List;
+public class ArchivedTasks extends AppCompatActivity {
+
+    LinearLayout archivedContainer;
+    List<Task> taskList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.archived_tasks);
+
+        archivedContainer = findViewById(R.id.archived_container);
+
+        taskList = TaskStorage.loadTasks(this);
+
+        for (Task t : taskList) {
+
+            if (t.isArchived) {
+                addArchivedCard(t);
+            }
+        }
+    }
+
+    private void addArchivedCard(Task task) {
+        View v = getLayoutInflater().inflate(R.layout.archived_card, null);
+
+        TextView taskName = v.findViewById(R.id.archived_task_name);
+        taskName.setText(task.name);
+
+        archivedContainer.addView(v);
+    }
+}
